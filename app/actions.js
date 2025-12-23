0b144a4cb5727dcdec27f76ce715a70df55b3a9c
shahnazfathima-official <shahnazfathima.official.com>
2025-12-23 18:36:03 +0530

    Checked_at modified

--- app/actions.js
+++ app/actions.js
@@ -115,7 +115,7 @@ export async function getProducts() {
     const { data, error } = await supabase
       .from("products")
       .select("*")
-      .order("checked_at", { ascending: false });
+      .order("created_at", { ascending: false });
 
     if (error) throw error;
     return data || [];
